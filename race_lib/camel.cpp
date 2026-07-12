22467287247aba9cae3ec45bd98e0f016a1794d5
Morloking <dyubenkosergei@yandex.ru>
2026-07-12 03:17:36 +0300

    Update camel.cpp

--- race_lib/camel.cpp
+++ race_lib/camel.cpp
@@ -6,7 +6,7 @@ namespace race_simulator {
 
     double Camel::calcDurationOfRest(double distance) {
         double allTime = distance / getSpeed();
-        int count = static_cast<int>((allTime - 1e-9) / 30);
+        int count = static_cast<int>(allTime / 30 + 1e-9);
         if (count == 0) return 0.0;
         else if (count == 1) return 5.0;
         else return 5.0 + (count - 1) * 8.0;
